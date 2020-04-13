34360235042cf749701a6a47d803e24ac604effa
Chris Rodriguez <melheffe@gmail.com>
2020-04-13 18:35:39 -0400

    Update latest tag getter Safeguard current branch Finally formatting release Notes ok Changing the post to use Authorization

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 function get_latest_tag {
-  latest_tag=$(git tag | tail -1)
+  latest_tag=$(git describe --abbrev=0 --tags)
+  echo "Latest Tag:" $latest_tag
 }
 
 function get_current_info {
-  current_branch=$TRIGGER
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  current_branch=${TRIGGER:=$branch}
+  echo "Trigger:" $current_branch
 }
 
 function prepare_file_info {
   version_on_file=$(cut -d " " -f 1 $file_path)
   if [[ $version_on_file == v* ]]; then version_on_file=$(echo $version_on_file | cut -c2-); fi
+  echo "Version on file:" $version_on_file
 }
 
 function prepare_github_info {
@@ -19,13 +23,26 @@ function prepare_github_info {
 }
 
 function set_release_notes {
-  release_notes=$(git log $latest_tag..HEAD --oneline --merges)
+  release_notes=$(git log $latest_tag..HEAD --merges --pretty=tformat:"%h %s <br/>")
   # Checking if the release notes are empty to get individual commits instead
-  if [ -z $release_notes ]; then release_notes=$(git log $latest_tag..HEAD --oneline); fi
+  if [ -z $release_notes ]; then release_notes=$(git log $latest_tag..HEAD --pretty=tformat:"%h %s <br/>"); fi
+  echo "Release Notes:" $release_notes
 }
 
 function create_git_tag_and_release {
-  curl --data  '{"tag_name": "'"$PREPEND$version_on_file$APPEND"'","target_commitish": "'"$current_branch"'","name": "'"Release $version_on_file"'","body": "'"Release $version_on_file"'","draft": false,"prerelease": true}' https://api.github.com/repos/$REPO_OWNER/$repo/releases?access_token=$TOKEN
+  # POST a release to repo via Github API
+  curl -s -X POST https://api.github.com/repos/$REPO_OWNER/$repo/releases \
+  -H "Authorization: token $TOKEN" \
+  -d @- << EOF
+  {
+  "tag_name": "$PREPEND$version_on_file$APPEND",
+  "target_commitish": "$current_branch",
+  "name": "Release $version_on_file",
+  "body": "$release_notes",
+  "draft": $DRAFT,
+  "prerelease": $PRERELEASE
+  }
+EOF
 }
 
 function validate_version_file {
@@ -41,8 +58,8 @@ echo "------------- Script Starting ----------------------"
 git fetch --prune-tags
 
 validate_version_file
-
 get_latest_tag
+get_current_info
 
 files=$(git diff --name-status $latest_tag HEAD | grep $VERSION_FILE_NAME)
 
@@ -54,7 +71,10 @@ else
   echo "Version File has been updated, proceeding to tag"
   prepare_file_info
   prepare_github_info
+  set_release_notes
   create_git_tag_and_release
+  result=$(create_git_tag_and_release)
+  echo $result | jq .url
   exit $?
 fi
 echo "------------- Script Ending ----------------------"
